466dbd2fad7da6cf99120ff3efaf6e537e56d7d8
visionmedia <tj@vision-media.ca>
2010-02-25 09:50:08 -0800

    Added string support

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -13,10 +13,17 @@ describe 'yaml'
     end
   end
   
-  describe '\d+'
+  describe 'int'
     it 'should convert to a number'
       var doc = yaml.eval('1')
       doc.should.eql 1
+    end
+  end
+  
+  describe '"string"'
+    it 'should convert to a string'
+      var doc = yaml.eval('"foo"')
+      doc.should.eql 'foo'
     end
   end
 end
