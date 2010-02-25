916a0a5be334c466bea5767d8452ca1ae77d8053
visionmedia <tj@vision-media.ca>
2010-02-25 09:33:25 -0800

    Converting ints to numbers

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -9,7 +9,14 @@ describe 'yaml'
   describe '-'
     it 'should convert to an array'
       var doc = yaml.eval('- 1\n- 2\n- 3\n')
-      doc.should.eql ['1', '2', '3']
+      doc.should.eql [1,2,3]
+    end
+  end
+  
+  describe '\d+'
+    it 'should convert to a number'
+      var doc = yaml.eval('1')
+      doc.should.eql 1
     end
   end
 end
