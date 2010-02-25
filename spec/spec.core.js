25283a010df8ef3c737d31f7fbec3616bb1d66d3
visionmedia <tj@vision-media.ca>
2010-02-25 10:12:53 -0800

    Added hash

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -6,13 +6,6 @@ describe 'yaml'
     end
   end
   
-  describe '-'
-    it 'should convert to an array'
-      var doc = yaml.eval('- 1  \n- 2\n- 3\n')
-      doc.should.eql [1,2,3]
-    end
-  end
-  
   describe 'int'
     it 'should convert to a number'
       var doc = yaml.eval('1')
@@ -24,6 +17,20 @@ describe 'yaml'
     it 'should convert to a string'
       var doc = yaml.eval('"foo"')
       doc.should.eql 'foo'
+    end
+  end
+  
+  describe '-'
+    it 'should convert to an array'
+      var doc = yaml.eval('- 1  \n- 2\n- 3\n')
+      doc.should.eql [1,2,3]
+    end
+  end
+  
+  describe 'key: val'
+    it 'should convert to an object'
+      var doc = yaml.eval('a: 1\nb: 2\n')
+      doc.should.eql { a: 1, b: 2 }
     end
   end
 end
