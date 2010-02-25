2d65e4592e5f433c3bca63eb7a4d171ee150e914
visionmedia <tj@vision-media.ca>
2010-02-25 13:03:21 -0800

    Uncommented specs

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -6,111 +6,111 @@ describe 'yaml'
     }
   end
   
-  // describe '.version'
-  //   it 'should be a triplet'
-  //     yaml.version.should.match(/^\d+\.\d+\.\d+$/)
-  //   end
-  // end
+  describe '.version'
+    it 'should be a triplet'
+      yaml.version.should.match(/^\d+\.\d+\.\d+$/)
+    end
+  end
   
-  // describe 'lexer'
-  //   describe 'indentation'
-  //     it 'should work'
-  //       var tokens = yaml.tokenize('\n  1\n  2\n    3\n      4\n')
-  //       tokens[0][0].should.eql 'indent'
-  //       tokens[1][0].should.eql 'int'
-  //       tokens[2][0].should.eql 'int'
-  //       tokens[3][0].should.eql 'indent'
-  //       tokens[4][0].should.eql 'int'
-  //       tokens[5][0].should.eql 'indent'
-  //       tokens[6][0].should.eql 'int'
-  //       tokens[7][0].should.eql 'dedent'
-  //       tokens[8][0].should.eql 'dedent'
-  //       tokens[9][0].should.eql 'dedent'
-  //     end
-  //   end
-  // end
+  describe 'lexer'
+    describe 'indentation'
+      it 'should work'
+        var tokens = yaml.tokenize('\n  1\n  2\n    3\n      4\n')
+        tokens[0][0].should.eql 'indent'
+        tokens[1][0].should.eql 'int'
+        tokens[2][0].should.eql 'int'
+        tokens[3][0].should.eql 'indent'
+        tokens[4][0].should.eql 'int'
+        tokens[5][0].should.eql 'indent'
+        tokens[6][0].should.eql 'int'
+        tokens[7][0].should.eql 'dedent'
+        tokens[8][0].should.eql 'dedent'
+        tokens[9][0].should.eql 'dedent'
+      end
+    end
+  end
   
-  // 
-  // describe 'int'
-  //   it 'should convert to a number'
-  //     yaml.eval('1').should.equal 1
-  //   end
-  // end
-  // 
-  // describe 'float'
-  //   it 'should convert to a number'
-  //     yaml.eval('1.5').should.equal 1.5
-  //   end
-  // end
-  // 
-  // describe '"string"'
-  //   it 'should convert to a string'
-  //     var doc = yaml.eval('"foo"')
-  //     doc.should.eql 'foo'
-  //   end
-  // end
-  // 
-  // describe '-'
-  //   it 'should convert to an array'
-  //     var doc = yaml.eval('- 1  \n- 2\n- 3\n')
-  //     doc.should.eql [1,2,3]
-  //   end
-  //   
-  //   it 'should support nesting lists'
-  //     var doc = yaml.eval('-\n  - 1\n  - 2\n')
-  //     doc.should.eql [[1,2]]
-  //   end
-  // end
-  // 
-  // describe 'key: val'
-  //   it 'should convert to an object'
-  //     var doc = yaml.eval('a: 1\nb:  2 \n')
-  //     doc.should.eql { a: 1, b: 2 }
-  //   end
-  // end
-  // 
-  // describe '# foo'
-  //   it 'should be ignored as a comment'
-  //     yaml.eval('# foo').should.be_undefined
-  //     yaml.eval('1 # foo').should.equal 1
-  //   end
-  // end
-  // 
-  // describe 'indentation'
-  //   describe 'when invalid'
-  //     it 'should throw an error'
-  //       -{ yaml.eval(fixture('list.invalid.yml')) }.should.throw_error 'invalid indentation, got 3 instead of 2'
-  //     end
-  //   end
-  // end
-  // 
+  
+  describe 'int'
+    it 'should convert to a number'
+      yaml.eval('1').should.equal 1
+    end
+  end
+  
+  describe 'float'
+    it 'should convert to a number'
+      yaml.eval('1.5').should.equal 1.5
+    end
+  end
+  
+  describe '"string"'
+    it 'should convert to a string'
+      var doc = yaml.eval('"foo"')
+      doc.should.eql 'foo'
+    end
+  end
+  
+  describe '-'
+    it 'should convert to an array'
+      var doc = yaml.eval('- 1  \n- 2\n- 3\n')
+      doc.should.eql [1,2,3]
+    end
+    
+    it 'should support nesting lists'
+      var doc = yaml.eval('-\n  - 1\n  - 2\n')
+      doc.should.eql [[1,2]]
+    end
+  end
+  
+  describe 'key: val'
+    it 'should convert to an object'
+      var doc = yaml.eval('a: 1\nb:  2 \n')
+      doc.should.eql { a: 1, b: 2 }
+    end
+  end
+  
+  describe '# foo'
+    it 'should be ignored as a comment'
+      yaml.eval('# foo').should.be_undefined
+      yaml.eval('1 # foo').should.equal 1
+    end
+  end
+  
+  describe 'indentation'
+    describe 'when invalid'
+      it 'should throw an error'
+        -{ yaml.eval(fixture('list.invalid.yml')) }.should.throw_error 'invalid indentation, got 3 instead of 2'
+      end
+    end
+  end
+  
   
   describe 'list'
-    // describe 'with nested list'
-    //   it 'should work'
-    //     assert('list.list', [[1,2,3,[4,5]]])
-    //   end
-    // end
+    describe 'with nested list'
+      it 'should work'
+        assert('list.list', [[1,2,3,[4,5]]])
+      end
+    end
     
     describe 'with nested lists'
       it 'should work'
-        assert('list.lists', [[1,2], [3,4, [5]], [6]])
+        assert('list.lists', [[1,2], [3,4, [5]]])
       end
     end
   end
 
-  // 
-  // describe 'hash with list'
-  //   it 'should work'
-  //     var expected = { pets: ['niko', 'simon'] }
-  //     yaml.eval(fixture('hash.list.yml')).should.eql expected
-  //   end
-  // end
-  // 
-  // describe 'hash with hash'
-  //   it 'should work'
-  //     var expected = { pets: { niko: 2, simon: 14 }}
-  //     yaml.eval(fixture('hash.hash.yml')).should.eql expected
-  //   end
-  // end
+  
+  describe 'hash with list'
+    it 'should work'
+      var expected = { pets: ['niko', 'simon'] }
+      yaml.eval(fixture('hash.list.yml')).should.eql expected
+    end
+  end
+  
+  describe 'hash with hash'
+    it 'should work'
+      var expected = { pets: { niko: 2, simon: 14 }}
+      yaml.eval(fixture('hash.hash.yml')).should.eql expected
+    end
+  end
 end
